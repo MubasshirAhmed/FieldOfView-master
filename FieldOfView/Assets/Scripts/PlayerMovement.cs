ab1b4d3550871b356ac6b25bdf86a78c7e1873c8
Mubasshir Ahmed <mubasshirahmed26@gmail.com>
2017-09-30 10:51:51 +0600

    new changes

---
+++ FieldOfView/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;
+    private float rotateSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0, horizontal * rotateSpeed, 0);
+    }
+}
